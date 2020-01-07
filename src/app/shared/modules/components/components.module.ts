8d5c74a294ebb7e2eca2840adaa2ed0f33b90085
Emirhan Erturk <emirhan@rapsodo.com>
2020-01-08 00:17:10 +0300

    created breadcrumb

--- src/app/shared/modules/components/components.module.ts
+++ src/app/shared/modules/components/components.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { LoaderComponent } from '@shared/components/loader/loader.component';
 import { ErrorComponent } from '@shared/components/error/error.component';
+import { BreadcrumbComponent } from '@shared/components/breadcrumb/breadcrumb.component';
 
 @NgModule({
   imports: [
@@ -11,10 +12,12 @@ import { ErrorComponent } from '@shared/components/error/error.component';
   declarations: [
     LoaderComponent,
     ErrorComponent,
+    BreadcrumbComponent,
   ],
   exports: [
     LoaderComponent,
     ErrorComponent,
+    BreadcrumbComponent,
   ]
 })
 export class ComponentsModule { }
