d1cc707d2dc92dc97056fb88f309e4119528d4d2
Emirhan Erturk <emirhan@rapsodo.com>
2021-04-28 23:16:44 +0300

    small fixes

--- src/app/shared/modules/components/components.module.ts
+++ src/app/shared/modules/components/components.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { LoaderComponent } from '@shared/components/loader/loader.component';
 import { ErrorComponent } from '@shared/components/error/error.component';
