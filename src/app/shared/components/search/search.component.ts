e1bb48619f23a4cff7e60d79de649f3261ca05a4
Emirhan Erturk <emirhan@rapsodo.com>
2022-01-15 23:05:54 +0300

    added articles on search

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -3,132 +3,114 @@ import { trigger, transition, style, animate } from '@angular/animations';
 
 import { AppService } from '@services/app.service';
 import { ICountry, IAthlete, IChamps } from '@interfaces/models.interface';
+import { Article } from '@models/article.model';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
   animations: [
-    trigger(
-      'fadeIn', 
-      [
-        transition(
-          ':enter', 
-          [
-            style({ opacity: 0 }),
-            animate('.3s ease-out', style({ opacity: 1 }))
-          ]
-        ),
-        transition(
-          ':leave', 
-          [
-            style({ opacity: 1 }),
-            animate('.3s ease-in', style({ opacity: 0 }))
-          ]
-        )
-      ]
-    ),
-    trigger(
-      'fadeInInner', 
-      [
-        transition(
-          ':enter', 
-          [
-            style({ opacity: 0 }),
-            animate('.3s ease-out', style({ opacity: 1 }))
-          ]
-        )
-      ]
-    )
-  ]
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('.3s ease-out', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('.3s ease-in', style({ opacity: 0 })),
+      ]),
+    ]),
+    trigger('fadeInInner', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('.3s ease-out', style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class SearchComponent implements OnInit {
-
-  opened: boolean = false;
-  loading: boolean = false;
+  opened = false;
+  loading = false;
 
   athletes: IAthlete[] = [];
+  articles: Article[] = [];
   countries: ICountry[] = [];
   champs: IChamps[] = [];
 
-  key: string = '';
-  popular: boolean = true;
+  key = '';
+  popular = true;
   timeout: any;
 
   constructor(
     private renderer: Renderer2,
     private appService: AppService
   ) {
-    
-    this.countries = [{
-      code: 'KEN',
-      name: 'Kenya'
-    },{
-      code: 'JAM',
-      name: 'Jamaica'
-    },{
-      code: 'TUR',
-      name: 'Turkey'
-    }];
+    this.countries = [
+      {
+        code: 'KEN',
+        name: 'Kenya',
+      },
+      {
+        code: 'JAM',
+        name: 'Jamaica',
+      },
+      {
+        code: 'TUR',
+        name: 'Turkey',
+      },
+    ];
 
-    this.champs = [{
-      id: 40,
-      name: "Olympic Games",
-      slug: "olympic-games"
-    }, {
-      id: 52,
-      name: "World Championships",
-      slug: "world-champs",
-    }, {
-      id: 18,
-      name: "European Champs",
-      slug: "european-champs"
-    }]
-
+    this.champs = [
+      {
+        id: 40,
+        name: 'Olympic Games',
+        slug: 'olympic-games',
+      },
+      {
+        id: 52,
+        name: 'World Championships',
+        slug: 'world-champs',
+      },
+      {
+        id: 18,
+        name: 'European Champs',
+        slug: 'european-champs',
+      },
+    ];
   }
 
-  ngOnInit() {
-
+  ngOnInit(): void {
     this.appService.search.subscribe(() => {
       this.renderer.addClass(document.body, 'fixed-body');
       this.opened = true;
     });
-
   }
 
-  search(e: any){
-
+  search(e: any): void {
     if (this.key === e.target.value.trim()) return;
 
     this.key = e.target.value.trim();
-    if (this.key.length > 2){
-
+    if (this.key.length > 2) {
       this.loading = true;
 
       clearTimeout(this.timeout);
       this.timeout = setTimeout(async () => {
-      
         const res = await this.appService.searchOnSite(this.key);
-        if (res.success){
+        if (res.success) {
           this.popular = false;
           this.athletes = res.data.athletes;
+          this.articles = res.data.articles;
           this.countries = res.data.countries;
           this.champs = res.data.champs;
         }
 
         this.loading = false;
-
       }, 200);
-
     }
-
   }
 
-  close(){
-
+  close(): void {
     this.renderer.removeClass(document.body, 'fixed-body');
     this.opened = false;
-
   }
-
 }
