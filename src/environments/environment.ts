f146a4c2fa73099c91596d48646d6e0d732c9530
Emirhan Ertuk <emirhanerturk@gmail.com>
2024-09-09 20:37:03 +0300

    refactor: update angular.json

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,8 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   api: {
-    host: 'http://localhost:3001',
+    host: 'https://api.athleticspodium.com',
+    // host: 'http://localhost:3001',
     version: '1.0',
   },
   cdn: {
@@ -14,24 +11,15 @@ export const environment = {
       athletes: 'media/athletes',
       champs: 'media/champs',
       articles: 'media/articles',
-    }
+    },
   },
   analytics: {
-    trackerId: 'G-7EDH9146FP'
+    trackerId: 'G-7EDH9146FP',
   },
   socialMedia: {
     // twitter: 'athleticspodium',
     mastadon: 'athleticspodium',
     facebook: 'athleticspodium',
-    instagram: 'athleticspodium'
+    instagram: 'athleticspodium',
   },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
